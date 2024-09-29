6466a4cdedbd40cd5a256266c324e10f6b327b7c
mswat5 <mswat14361@gmail.com>
2024-09-29 21:07:27 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import abhaRoutes from "./routes/abharoutes.js";
-
+import cors from "cors";
+app.use(cors());
 dotenv.config();
 
 const app = express();
