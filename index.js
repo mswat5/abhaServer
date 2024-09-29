ca0f9d9e496ae62b5aaaff4ff6da2632ea7e6dca
mswat5 <mswat14361@gmail.com>
2024-09-29 22:24:17 +0530

    minor fix

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import abhaRoutes from "./routes/abharoutes.js";
 import cors from "cors";
-app.use(cors());
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/", abhaRoutes);
 
 const PORT = process.env.PORT;
